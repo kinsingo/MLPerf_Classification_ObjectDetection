aa8c2dbc1e6e7e1717ba4a5730ecf60a6c2c6005
jonghyun shin <jonghyun_shin@naver.com>
2024-09-19 18:24:54 +0800

    240919V2

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Progress
+- 24.09.19
+ 1. Repo 생성 (Classfication 이랑 ObjectDetection 만 저장)
+ 2. OrangePi 를 SSH 통해서 Window 환경에서 접근 가능하도록 함 (orangepi@147.46.168.115:8024)
+ 3. dataset 경로는 "home/orangepi/dataset/imageNet2012-validation" 여기임, 각 class 별로 하나의 폴더, 각 폴더는 50 Samples (50000 = 1000 x 50)
+ 
---
+++ imageNet_example.py
@@ -0,0 +1,59 @@
+import os
+import cv2
+import numpy as np
+import json
+import argparse
+from dx_engine import InferenceEngine
+
+    
+def preprocessing(image, new_shape=(224, 224), align=64, format=None):
+    image = cv2.resize(image, new_shape)
+    h, w, c = image.shape
+    if format is not None:
+        image = cv2.cvtColor(image, format)
+    if align == 0 :
+        return image
+    length = w * c
+    align_factor = align - (length - (length & (-align)))
+    image = np.reshape(image, (h, w * c))
+    dummy = np.full([h, align_factor], 0, dtype=np.uint8)
+    image_input = np.concatenate([image, dummy], axis=-1)cd 
+        
+    return image_input
+
+
+def run_example(config):
+    model_path = config["model"]["path"]
+    classes = config["output"]["classes"]
+    input_list = []
+    
+    for source in config["input"]["sources"]:
+        if source["type"] == "image":
+            input_list.append(source["path"])
+    if len(input_list) == 0:
+        input_list.append("example/ILSVRC2012/0.jpeg")
+    
+    ie = InferenceEngine(model_path)
+    
+    for input_path in input_list:
+        image_src = cv2.imread(input_path, cv2.IMREAD_COLOR)
+        image_input = preprocessing(image_src, new_shape=(224, 224), align=64, format=cv2.COLOR_BGR2RGB)
+        ie_output = ie.run(image_input)
+        
+        print("[{}] Top1 Result : class {} ({})".format(input_path, ie_output[0][0], classes[ie_output[0][0]]))
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='./example/imagenet_example.json', type=str, help='yolo object detection json config path')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.config):
+        parser.print_help()
+        exit()
+    
+    with open(args.config, "r") as f:
+        json_config = json.load(f)
+        
+    run_example(json_config)
